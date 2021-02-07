2495538b3c0783de74a1996c6796bafda1d9b3be
Thomas Perale <perale.thomas@gmail.com>
2021-02-07 16:38:58 +0100

    posts: import failsafe when no content

--- src/utils/content.js
+++ src/utils/content.js
@@ -15,16 +15,17 @@ export function makeUrl(article) {
  */
 export function filterPosts(summaryJson) {
   return (
-    summaryJson &&
-    summaryJson.fileMap &&
-    Object.keys(summaryJson.fileMap)
-      .filter((file) => file.indexOf(POST_SOURCE) === 0)
-      .map((file) => summaryJson.fileMap[file])
-      .map((post) => Object.assign({}, post, { href: makeUrl(post) }))
-      .sort((a, b) => {
-        const aDate = Date.parse(a.date);
-        const bDate = Date.parse(b.date);
-        return bDate > aDate ? 1 : bDate < aDate ? -1 : 0;
-      })
+    (summaryJson &&
+      summaryJson.fileMap &&
+      Object.keys(summaryJson.fileMap)
+        .filter((file) => file.indexOf(POST_SOURCE) === 0)
+        .map((file) => summaryJson.fileMap[file])
+        .map((post) => Object.assign({}, post, { href: makeUrl(post) }))
+        .sort((a, b) => {
+          const aDate = Date.parse(a.date);
+          const bDate = Date.parse(b.date);
+          return bDate > aDate ? 1 : bDate < aDate ? -1 : 0;
+        })) ||
+    []
   );
 }
