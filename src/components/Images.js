82171521c4b9dac9da9a8c83c7466e4605ea82df
Thomas Perale <perale.thomas@gmail.com>
2021-02-07 16:35:17 +0100

    Image: css linting

--- src/components/Images.js
+++ src/components/Images.js
@@ -29,36 +29,36 @@ export const Gradient = styled.div`
     display: inline-block;
     background: -moz-linear-gradient(
       top,
-      rgba(${props => props.theme.primary}, 1) 0%,
+      rgba(${(props) => props.theme.primary}, 1) 0%,
       rgba(255, 255, 255, 0) 50%
-    ); /* FF3.6+ */
+    );
     background: -webkit-gradient(
       linear,
       left top,
       left bottom,
       color-stop(0%, rgba(255, 255, 255, 0)),
-      color-stop(100%, rgba(${props => props.theme.primary}, 1))
-    ); /* Chrome,Safari4+ */
+      color-stop(100%, rgba(${(props) => props.theme.primary}, 1))
+    );
     background: -webkit-linear-gradient(
       top,
-      rgba(${props => props.theme.primary}, 1) 0%,
+      rgba(${(props) => props.theme.primary}, 1) 0%,
       rgba(255, 255, 255, 0) 50%
     ); /* Chrome10+,Safari5.1+ */
     background: -o-linear-gradient(
       top,
-      rgba(${props => props.theme.primary}, 1) 0%,
+      rgba(${(props) => props.theme.primary}, 1) 0%,
       rgba(255, 255, 255, 0) 50%
-    ); /* Opera 11.10+ */
+    );
     background: -ms-linear-gradient(
       top,
-      rgba(${props => props.theme.primary}, 1) 0%,
+      rgba(${(props) => props.theme.primary}, 1) 0%,
       rgba(255, 255, 255, 0) 50%
-    ); /* IE10+ */
+    );
     background: linear-gradient(
       to bottom,
-      rgba(${props => props.theme.primary}, 1) 0%,
+      rgba(${(props) => props.theme.primary}, 1) 0%,
       rgba(255, 255, 255, 0) 50%
-    ); /* W3C */
+    );
   }
 `;
 
@@ -75,7 +75,7 @@ export const Overlay = styled.span`
     width: 100%;
     height: 100%;
     display: inline-block;
-    background: ${props => props.theme.dark};
+    background: ${(props) => props.theme.dark};
     opacity: 0.5;
   }
 `;
